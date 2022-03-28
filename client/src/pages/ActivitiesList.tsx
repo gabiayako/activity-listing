c48c5f9671b2f6101e3260241876c68085b29f43
gabiayako <gabiayako@gmail.com>
2022-03-28 17:24:37 -0300

    Revert "using react-query"
    This reverts commit 95ed33d0dfa16dfc686ab8cba1752a5aee3d219e.

--- client/src/pages/ActivitiesList.tsx
+++ client/src/pages/ActivitiesList.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
 import { Heading } from "@chakra-ui/react";
 
 import { PageWrapper, ActivityCard } from "../components";
@@ -10,17 +10,18 @@ interface Activity {
   studentGroup: string;
 }
 
-const fetchActivities = async () => {
-  const response = await fetch("http://localhost:9000/activities");
-  const data = await response.json();
-  return data;
-};
-
 function ActivitiesList() {
-  const { data: activities } = useQuery<Activity[]>(
-    "activities",
-    fetchActivities
-  );
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      const response = await fetch("http://localhost:9000/activities");
+      const data = await response.json();
+      setActivities(data);
+    };
+
+    fetchActivities();
+  }, []);
 
   return (
     <PageWrapper>
