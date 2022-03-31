879c1e6a55431e768c84791cd90e6916a2e0a820
gabiayako <gabiayako@gmail.com>
2022-03-31 19:35:16 -0300

    Adding report-data to activity card in activity list

--- client/src/pages/ActivitiesList.tsx
+++ client/src/pages/ActivitiesList.tsx
@@ -3,6 +3,8 @@ import { Heading } from "@chakra-ui/react";
 
 import { PageWrapper, ActivityCard } from "../components";
 
+import { getMeanGradeAndParticipation } from "../utils";
+
 interface Activity {
   id: string;
   subject: string;
@@ -16,22 +18,50 @@ const fetchActivities = async () => {
   return data;
 };
 
+interface Report {
+  activityId: string;
+  studentData: { name: string; grade: number | null }[];
+}
+
+const fetchReport = async () => {
+  const response = await fetch("http://localhost:9000/reports");
+  const data = await response.json();
+  return data;
+};
+
 function ActivitiesList() {
   const { data: activities } = useQuery<Activity[]>(
     "activities",
-    fetchActivities
+    fetchActivities,
+    {
+      staleTime: 1000 * 60,
+    }
   );
+
+  const { data: reports } = useQuery<Report[]>("reports", fetchReport, {
+    staleTime: 1000 * 60,
+  });
 
   return (
     <PageWrapper>
       <Heading mb={4}>Atividades</Heading>
       {activities
         ? activities.map(({ id, subject, chapter, studentGroup }) => {
+            const report = (reports || []).find(
+              (report) => report.activityId === id
+            );
+
+            const { meanGrade, participation } = getMeanGradeAndParticipation(
+              report?.studentData
+            );
+
             return (
               <ActivityCard
                 key={id}
                 activityId={id}
                 chapter={chapter}
+                meanGrade={meanGrade}
+                participation={participation}
                 studentGroup={studentGroup}
                 subject={subject}
               />
