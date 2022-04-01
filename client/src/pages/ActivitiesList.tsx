a681084acd6f78a02788a9b81fc0fc533622eab3
gabiayako <gabiayako@gmail.com>
2022-04-01 15:26:33 -0300

    fix base code

--- client/src/pages/ActivitiesList.tsx
+++ client/src/pages/ActivitiesList.tsx
@@ -1,37 +1,34 @@
-import { useQuery } from "react-query";
 import { Heading } from "@chakra-ui/react";
 
 import { PageWrapper, ActivityCard } from "../components";
-
-interface Activity {
-  id: string;
-  subject: string;
-  chapter: string;
-  studentGroup: string;
-}
-
-const fetchActivities = async () => {
-  const response = await fetch("http://localhost:9000/activities");
-  const data = await response.json();
-  return data;
-};
+import { useActivities, useReports } from "../hooks";
+import { getMeanGradeAndParticipation } from "../utils";
 
 function ActivitiesList() {
-  const { data: activities } = useQuery<Activity[]>(
-    "activities",
-    fetchActivities
-  );
+  const { data: activities } = useActivities();
+  const { data: reports, isLoading: isReportLoading } = useReports();
 
   return (
     <PageWrapper>
       <Heading mb={4}>Atividades</Heading>
       {activities
         ? activities.map(({ id, subject, chapter, studentGroup }) => {
+            const report = (reports || []).find(
+              (report) => report.activityId === id
+            );
+
+            const { meanGrade, participation } = getMeanGradeAndParticipation(
+              report?.studentData
+            );
+
             return (
               <ActivityCard
                 key={id}
                 activityId={id}
                 chapter={chapter}
+                isReportLoading={isReportLoading}
+                meanGrade={meanGrade}
+                participation={participation}
                 studentGroup={studentGroup}
                 subject={subject}
               />
