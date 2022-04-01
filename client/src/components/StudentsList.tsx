9b63600a3fd5887d14dee8b084ba26a9d0437749
gabiayako <gabiayako@gmail.com>
2022-04-01 16:28:09 -0300

    add prefetch when rendering student list

--- client/src/components/StudentsList.tsx
+++ client/src/components/StudentsList.tsx
@@ -21,35 +21,48 @@ export const StudentsList = ({
       </Thead>
 
       <Tbody>
-        {studentData?.map(({ id: studentId, name, grade }) => (
-          <Tr key={name}>
-            <Td>
-              <Button
-                w="100%"
-                variant="link"
-                justifyContent="flex-start"
-                onMouseOver={async () => {
-                  await queryClient.prefetchQuery(
-                    ["student", studentId],
-                    () => fetchStudent(studentId),
-                    {
-                      staleTime: 60 * 1000, // only prefetch if older than 10 seconds
-                    }
-                  );
-                }}
-                onClick={() => {
-                  navigator("/student", {
-                    replace: false,
-                    state: { studentId },
-                  });
-                }}
-              >
-                {name}
-              </Button>
-            </Td>
-            <Td isNumeric>{grade}</Td>
-          </Tr>
-        ))}
+        {studentData?.map(({ id: studentId, name, grade }) => {
+          // const preFetchStudent = async () => {
+          //   await queryClient.prefetchQuery(
+          //     ["student", studentId],
+          //     () => fetchStudent(studentId),
+          //     {
+          //       staleTime: 60 * 1000, // only prefetch if older than 10 seconds
+          //     }
+          //   );
+          // };
+          // preFetchStudent();
+
+          return (
+            <Tr key={name}>
+              <Td>
+                <Button
+                  w="100%"
+                  variant="link"
+                  justifyContent="flex-start"
+                  // onMouseOver={async () => {
+                  //   await queryClient.prefetchQuery(
+                  //     ["student", studentId],
+                  //     () => fetchStudent(studentId),
+                  //     {
+                  //       staleTime: 60 * 1000, // only prefetch if older than 60 seconds
+                  //     }
+                  //   );
+                  // }}
+                  onClick={() => {
+                    navigator("/student", {
+                      replace: false,
+                      state: { studentId },
+                    });
+                  }}
+                >
+                  {name}
+                </Button>
+              </Td>
+              <Td isNumeric>{grade}</Td>
+            </Tr>
+          );
+        })}
       </Tbody>
     </Table>
   ) : null;
--- client/src/pages/Student.tsx
+++ client/src/pages/Student.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Heading, Stack } from "@chakra-ui/react";
+import { Avatar, Heading, Spinner, Stack } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import { PageWrapper } from "../components";
 import { useStudent } from "../hooks";
@@ -11,19 +11,30 @@ function Student() {
   const { state } = useLocation();
   const { studentId } = state as LocationState;
 
-  const { data: student } = useStudent(studentId);
+  const { data: student, isLoading } = useStudent(studentId);
 
   return (
     <PageWrapper>
-      <Stack
-        spacing="1rem"
-        h="100%"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Avatar size="2xl" name={student.name} src={student.picture} />
-        <Heading>{student.name}</Heading>
-      </Stack>
+      {isLoading ? (
+        <Stack h="100%" alignItems="center" justifyContent="center">
+          <Spinner />
+        </Stack>
+      ) : (
+        <Stack
+          spacing="1rem"
+          h="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Avatar
+            loading="lazy"
+            size="2xl"
+            name={student.name}
+            src={student.picture}
+          />
+          <Heading>{student.name}</Heading>
+        </Stack>
+      )}
     </PageWrapper>
   );
 }
